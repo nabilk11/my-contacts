cbdab3beff60a465643c1e1e20abebb909e5ff8c
nabilk11 <khan.nabiln11.com>
2023-02-11 13:13:42 -0500

    added jwt auth and dotenv for user login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,9 @@
+import * as dotenv from "dotenv";
+dotenv.config({ path: "./config/config.env" });
+
 import { Router } from "express";
 import * as bcrypt from "bcrypt";
+import jsonwebtoken from "jsonwebtoken";
 
 import { User } from "../models/User.js";
 
@@ -7,6 +11,7 @@ const router = Router();
 
 router.post("/login");
 
+// User Register Router
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
 
@@ -48,6 +53,52 @@ router.post("/register", async (req, res) => {
     const result = await newUser.save();
 
     return res.status(201).json({ ...result._doc });
+  } catch (err) {
+    console.log(`Error: ${err}`);
+    return res.status(500).json({ error: err.message });
+  }
+});
+
+// User Login Route
+router.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password)
+    return res.status(400).json({ error: "All fields are required!" });
+
+  // Check for valid email
+  const validEmailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (!validEmailRegex.test(email))
+    return res.status(400).json({ error: "Invalid Email Format!" });
+
+  try {
+    // Check for existing user
+    const existingUser = await User.findOne({ email });
+
+    // If no user exists
+    if (!existingUser)
+      return res.status(400).json({ error: "Invalid Username or Password!" });
+
+    // If user exists - Decrypt password
+
+    const matchingPassword = await bcrypt.compare(
+      password,
+      existingUser.password
+    );
+
+    // If password does not match
+    if (!matchingPassword)
+      return res.status(400).json({ error: "Invalid Username or Password!" });
+
+    // JWT AUTH
+    const payload = { _id: existingUser._id };
+    const token = jsonwebtoken.sign(payload, process.env.JWT_SECRET_KEY, {
+      expiresIn: "1h",
+    });
+
+    return res.status(200).json({ token });
   } catch (err) {
     console.log(`Error: ${err}`);
     return res.status(500).json({ error: err.message });
