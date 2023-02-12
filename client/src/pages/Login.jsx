dbab6c0004632040bac28ecf887674e98f3b5b79
nabilk11 <khan.nabiln11.com>
2023-02-12 15:06:29 -0500

    modified page headers, and added basic login/register handling functions

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,12 +1,75 @@
-import React from 'react'
-import Header from '../components/Header'
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 const Login = () => {
+  // user login credentials
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(credentials)
+  };
+
   return (
     <div>
-    <Header title={'Login to Your Account'}/>
+      <Header title={"Login to Your Account"} />
+      <div className="login-form">
+        <form action="" onSubmit={handleLogin}>
+          <div className="form-group">
+            <label for="email-input" className="form-label mt-4">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="email@example.com"
+              value={credentials.email}
+              name="email"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              We will never share your email with anybody!
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="password-input" className="form-label mt-4">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="*********"
+              value={credentials.password}
+              name="password"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              Password must be at least 6 characters!
+            </small>
+          </div>
+          <button type="submit" class="btn btn-primary btn-lg">
+            Login
+          </button>
+        </form>
+        <span>
+          Don't have an account? <Link to={"/register"}>Register here!</Link>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,12 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../components/Header";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-        <Header title={'Register for an Account'}/>
-        </div>
-      )
-    }
+  // user registration credentials
+  const [credentials, setCredentials] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+    console.log(credentials);
+  };
+
+  return (
+    <div>
+      <Header title={"Create an Account"} />
+      <div className="register-form">
+        <form action="" onSubmit={handleRegister}>
+          <div className="form-group">
+            <label for="email-input" className="form-label mt-4">
+              Name
+            </label>
+            <input
+              type="name"
+              className="form-control"
+              id="name-field"
+              placeholder="Your Name"
+              value={credentials.name}
+              name="name"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              Name can be a maximum of 30 characters!
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="email-input" className="form-label mt-4">
+              Email Address
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email-field"
+              placeholder="email@example.com"
+              value={credentials.email}
+              name="email"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              We will never share your email with anybody!
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="password-input" className="form-label mt-4">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password-input"
+              placeholder="*********"
+              value={credentials.password}
+              name="password"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              Password must be at least 6 characters!
+            </small>
+          </div>
+          <div className="form-group">
+            <label for="password-input" className="form-label mt-4">
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="confirmPassword"
+              placeholder="*********"
+              value={credentials.confirmPassword}
+              name="confirmPassword"
+              onChange={handleChange}
+              required
+            />
+            <small id="form-sub-text" className="form-text text-muted">
+              Password must be at least 6 characters!
+            </small>
+          </div>
+          <button type="submit" class="btn btn-primary btn-lg">
+            Register
+          </button>
+        </form>
+        <span>
+          Already have an account? <Link to={"/login"}>Login here!</Link>
+        </span>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
